cc63bf6b56f3b645e86a900c52b85e0a2fdfb787
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-15 14:53:22 -0400

    Added users controller to API. added ability that user cannot ask a question without valid token

--- Quest/app/controllers/api/v1/questions_controller.rb
+++ Quest/app/controllers/api/v1/questions_controller.rb
@@ -15,4 +15,37 @@ class Api::V1::QuestionsController < ApplicationController
       render json: { error: "You don't have permission to access these resources" }, status: :unauthorized
     end
   end
+
+    before_action :set_question, only: [:show]
+  
+    def index
+      @questions = Question.page(params[:page])
+    end
+  
+    def show
+      @answers = @question.answers
+      @answer = Answer.new(params[:question_id])
+      render json: @answer
+    end
+  
+    def edit
+      @question = Question.find(params[:id])
+    end
+  
+    def create
+      @question = Question.new(question_params)
+  
+      if @question.save
+        render json: @question, status: :created, location: @question
+      else
+          render json: @question.errors, status: :unprocessable_entity
+      end
+    end
+  
+  
+    private
+  
+    def set_question
+      @question = Question.find(params[:id])
+    end
 end
