aee02cc7ffc36a13f833d3c3f032e95a812a6f3e
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-19 16:28:48 -0400

    Added backend search to question, answer, and user models. Console testing successful.

--- Quest/app/models/question.rb
+++ Quest/app/models/question.rb
@@ -2,4 +2,19 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
   validates :title, :body, presence: true
+
+  include PgSearch
+
+  pg_search_scope :search_name, against: :title
+  pg_search_scope :search_all_variants, 
+  against: [:title, :body], 
+    using: { 
+      tsearch: { dictionary: 'english' }
+    }
+  
+  pg_search_scope :search_all_partial_matches, 
+    against: [:title, :body], 
+    using: {
+      tsearch: { prefix: true }
+    }
 end
